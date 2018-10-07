25a7559966ad6efeac24d30e626ede0c1ad604f0
ymsang <ymsang@MP-EOC-0031macpro.local>
2018-10-07 22:59:02 +0800

    增加测试用例

--- test/index.test.js
+++ test/index.test.js
@@ -27,12 +27,40 @@ describe('类测试', function () {
       'de hua liu',
       'dhliu',
       'liudh',
-      'dhliu',
       'ldh',
       'dhl'
     ]
     for (let mode of modes) {
       p.go(mode, 'lower')[0].should.equal(mode)
+    }
+  })
+
+  it('人名+大小写完整测试', () => {
+    let name = '吕洞宾'
+    let testCases = [
+      { mode: 'liu de hua', letter: 'lower', expect: 'lv dong bin' },
+      { mode: 'liu dehua', letter: 'lower', expect: 'lv dongbin' },
+      { mode: 'dehua liu', letter: 'lower', expect: 'dongbin lv' },
+      { mode: 'de hua liu', letter: 'lower', expect: 'dong bin lv' },
+      { mode: 'dhliu', letter: 'lower', expect: 'dblv' },
+      { mode: 'liudh', letter: 'lower', expect: 'lvdb' },
+      { mode: 'ldh', letter: 'lower', expect: 'ldb' },
+      { mode: 'dhl', letter: 'lower', expect: 'dbl' },
+
+      { "mode": "liu de hua", "letter": "upper", "expect": "LV DONG BIN" },
+      { "mode": "liu dehua", "letter": "upper", "expect": "LV DONGBIN" },
+      { "mode": "dehua liu", "letter": "upper", "expect": "DONGBIN LV" },
+      { "mode": "de hua liu", "letter": "upper", "expect": "DONG BIN LV" },
+      { "mode": "dhliu", "letter": "upper", "expect": "DBLV" },
+      { "mode": "liudh", "letter": "upper", "expect": "LVDB" },
+      { "mode": "ldh", "letter": "upper", "expect": "LDB" },
+      { "mode": "dhl", "letter": "upper", "expect": "DBL" }
+    ]
+
+    let p = new PinyinConverter(name)
+    for (let c of testCases) {
+      let { mode, letter, expect } = c
+      p.go(mode, letter)[0].should.equal(expect)
     }
   })
 })
